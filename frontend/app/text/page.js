f512a320a4aab8b594cda9926c248ffb79142e99
Mingle <tattooanywhere@gmail.com>
2026-04-27 18:32:26 +0530

    fix: disable SSR for text chat page

--- frontend/app/text/page.js
+++ frontend/app/text/page.js
@@ -1,6 +1,10 @@
 'use client';
 
-import ChatRoom from '../../components/ChatRoom';
+import dynamic from 'next/dynamic';
+
+const ChatRoom = dynamic(() => import('../../components/ChatRoom'), {
+  ssr: false,
+});
 
 export default function TextChatPage() {
   return <ChatRoom />;
